60b1cc1621b7add0c586e309de3814530f9dc250
Konstantin Aleksandrov <alexandrovkm@gmail.com>
2019-01-10 14:56:42 +0700

    Change koa-router to gen-server-router

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const glob     	  	   = require('glob');
 const addToStaticAsync = require('@gen-server/utils/copyToStatic');
 // const removeFromStatic = require('@gen-server/utils/removeFromStatic');
-const Router      	   = require('koa-router');
+const Router      	   = require('@gen-server/lib/router');
 const router      	   = new Router();
 const frontpage   	   = require('./controllers/frontpage');
 
@@ -14,5 +14,6 @@ router
 	.get('/__userAgent__', async ctx => ctx.body = ctx.userAgent);
 
 module.exports = function (app) {
-	app.use(router.routes());
+    app.use(router.routes());
+    app.use(router.allowedMethods());
 };
